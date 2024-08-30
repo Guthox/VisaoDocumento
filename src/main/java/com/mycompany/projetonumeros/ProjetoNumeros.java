96c1bbd081719f4ec0e7bf7915c9dfc9f65c6b91
guthox <gutho.san@gmail.com>
2024-08-30 09:55:12 -0300

    Primeiro commit

---
+++ src/main/java/com/mycompany/projetonumeros/ProjetoNumeros.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetonumeros;
+
+/**
+ *
+ * @author Gutho
+ */
+public class ProjetoNumeros {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
